0a5732d5927fa9a3c988bd4b73e806031ef4a278
Dani <dalvarezr95@elpuig.xeill.net>
2019-09-30 18:50:37 +0200

    Toggle Button documentado

--- app/src/main/java/com/example/p4/MainActivity.java
+++ app/src/main/java/com/example/p4/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
     SharedPreferences prefs;
 
     ToggleButton toggleButton;
@@ -17,17 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // ----- TOGGLE BUTTON
         prefs = getPreferences(MODE_PRIVATE);
-
+        // Con esto pedimos el togglebutton al XML
         toggleButton =findViewById(R.id.toggleButton);
+        // Con esta linea indicamos que la variable de prefs estara en el archivo TOGGLEBUTTON, en el caso de que dicho archivo no exista el valor por defecto sera true
         toggleButton.setChecked(prefs.getBoolean("TOGGLEBUTTON", false));
-
+        // Con esta linea declaramos que al darle click al boton haga lo de las siguientes lineas
         toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            //Esto sirve para que guarde las variables en el archivo TOGGLEBUTTON
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                //Esto sirve para que guarde las variables en el archivo TOGGLEBUTTON
                 prefs.edit().putBoolean("TOGGLEBUTTON", b).apply();
             }
         });
+
+
+
+
+
+
+
     }
 }
