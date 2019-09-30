4d3ed9376468546c5dac6ad126e8430ac82f08d3
Dani <dalvarezr95@elpuig.xeill.net>
2019-09-30 18:38:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/p4/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.p4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences prefs;
+
+    ToggleButton toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prefs = getPreferences(MODE_PRIVATE);
+
+        toggleButton =findViewById(R.id.toggleButton);
+        toggleButton.setChecked(prefs.getBoolean("TOGGLEBUTTON", false));
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                prefs.edit().putBoolean("TOGGLEBUTTON", b).apply();
+            }
+        });
+    }
+}
